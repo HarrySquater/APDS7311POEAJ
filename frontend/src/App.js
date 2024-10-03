2fd91be727ae36c4bae87a1c60f17929bcbb661f
Adriano-Escola <adrianoescola96@gmail.com>
2024-10-03 11:55:05 +0200

    Created Login Page, Implemented login functionality.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/login'
+
 function App() {
-  return <div></div>
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <div className='pages'>
+          <Routes>
+            <Route path='/Login' element={<Login />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
---
+++ frontend/src/pages/login.js
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+import { useLogin } from '../hooks/useLogin'
+
+const Login = () => {
+  const [fullName, setFullName] = useState('')
+  const [accountNumber, setAccountNumber] = useState('')
+  const [password, setPassword] = useState('')
+  const { login, isLoading, error } = useLogin()
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    await login(fullName, accountNumber, password)
+  }
+
+  return (
+    <div>
+      <h1
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        Login
+      </h1>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '50vh',
+        }}
+      >
+        <form>
+          <label>
+            Full Name:
+            <input
+              type='fullName'
+              onChange={(e) => setFullName(e.target.value)}
+              value={fullName}
+            />
+          </label>
+          <br />
+          <label>
+            Account Number:
+            <input
+              type='accountNumber'
+              onChange={(e) => setAccountNumber(e.target.value)}
+              value={accountNumber}
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type='password'
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </label>
+          <br />
+          <button type='button' onClick={handleLogin} disabled={isLoading}>
+            Login
+          </button>
+          {error && <div className='error'>{error}</div>}
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
