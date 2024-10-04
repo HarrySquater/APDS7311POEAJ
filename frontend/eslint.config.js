9f9efcf51036dfd85a2400ff6e0e524a5e9b8233
Adriano-Escola <adrianoescola96@gmail.com>
2024-10-04 13:36:26 +0200

    Revert "Testing linting"
    This reverts commit aeb1718fa3da56508be15d214776fffb12e269ee.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,25 +1,13 @@
 module.exports = [
   {
-    files: ['*.js', '*.jsx'],
+    ignores: ['node_modules/**'],
     languageOptions: {
-      parser: '@babel/eslint-parser',
-      parserOptions: {
-        ecmaVersion: 2021,
-        sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
-    plugins: {
-      react: require('eslint-plugin-react'),
-      'react-hooks': require('eslint-plugin-react-hooks'),
+      ecmaVersion: 2021,
+      sourceType: 'module',
     },
     rules: {
-      'react/jsx-uses-react': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
+      'no-console': 'warn',
+      semi: 'off',
     },
   },
 ]
