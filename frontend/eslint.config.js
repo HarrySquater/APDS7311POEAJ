aeb1718fa3da56508be15d214776fffb12e269ee
Adriano-Escola <adrianoescola96@gmail.com>
2024-10-04 13:25:37 +0200

    Testing linting

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,13 +1,25 @@
 module.exports = [
   {
-    ignores: ['node_modules/**'],
+    files: ['*.js', '*.jsx'],
     languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
+      parser: '@babel/eslint-parser',
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react: require('eslint-plugin-react'),
+      'react-hooks': require('eslint-plugin-react-hooks'),
     },
     rules: {
-      'no-console': 'warn',
-      semi: 'off',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
     },
   },
 ]
