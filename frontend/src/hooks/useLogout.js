0a941e0df603482584e1bc7bce90ca6ba991d1a6
Adriano-Escola <adrianoescola96@gmail.com>
2024-10-07 12:38:31 +0200

    Created and implemented Logout

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -3,9 +3,21 @@ import { useAuthenticationContext } from './useAuthenticationContext'
 export const useLogout = () => {
   const { dispatch } = useAuthenticationContext()
 
-  const logout = () => {
-    localStorage.removeItem('user')
-    dispatch({ type: 'LOGOUT' })
+  const logout = async () => {
+    const response = await fetch('/api/user/logout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    if (response.ok) {
+      localStorage.removeItem('user')
+
+      dispatch({ type: 'LOGOUT' })
+    } else {
+      console.error('Failed to log out')
+    }
   }
 
   return { logout }
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,13 +1,26 @@
 import React from 'react'
+import { useLogout } from '../hooks/useLogout'
 
 function Home() {
+  const { logout } = useLogout()
+
+  const handleLogout = async () => {
+    await logout()
+    window.location.reload()
+  }
+
   return (
-    <div>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-end',
+      }}
+    >
+      <button onClick={handleLogout}>Logout</button>
       <h1
         style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
+          alignSelf: 'center',
           fontSize: '50px',
         }}
       >
