060c0393287c3df2158b9e0eddeb5450151a5922
Adriano-Escola <adrianoescola96@gmail.com>
2024-11-06 15:05:45 +0200

    Create useAdminLogout.js

---
+++ frontend/src/hooks/useAdminLogout.js
@@ -0,0 +1,24 @@
+import { useAdminAuthenticationContext } from '../contexts/AdminAuthenticationContext'
+
+export const useAdminLogout = () => {
+  const { dispatch } = useAdminAuthenticationContext()
+
+  const adminLogout = async () => {
+    const response = await fetch('/api/admin/adminlogout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    if (response.ok) {
+      localStorage.removeItem('admin')
+
+      dispatch({ type: 'ADMINLOGOUT' })
+    } else {
+      console.error('Failed to log out')
+    }
+  }
+
+  return { adminLogout }
+}
