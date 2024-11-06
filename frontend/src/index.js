11c141dfe9e892d191424c91731efefb9991460d
Adriano-Escola <adrianoescola96@gmail.com>
2024-11-06 15:13:47 +0200

    Refactored index to include admin

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { AuthenticationContextProvider } from './context/AuthenticationContext'
+import { AdminAuthenticationContextProvider } from './context/AdminAuthenticationContext'
 import { PaymentsContextProvider } from './context/PaymentContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <AuthenticationContextProvider>
-      <PaymentsContextProvider>
-        <App />
-      </PaymentsContextProvider>
+      <AdminAuthenticationContextProvider>
+        <PaymentsContextProvider>
+          <App />
+        </PaymentsContextProvider>
+      </AdminAuthenticationContextProvider>
     </AuthenticationContextProvider>
   </React.StrictMode>
 )
