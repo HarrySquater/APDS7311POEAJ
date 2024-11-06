df2643f6d706e6e213871e84ea7fbfafafd1575b
Adriano-Escola <adrianoescola96@gmail.com>
2024-11-06 14:40:08 +0200

    Created adminRouter

---
+++ backend/routes/adminRouter.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const {
+  loginAdmin,
+  signupAdmin,
+  logoutAdmin,
+} = require('../controllers/adminController')
+const ExpressBrute = require('express-brute')
+
+const router = express.Router()
+
+const store = new ExpressBrute.MemoryStore()
+const bruteForce = new ExpressBrute(store, {
+  freeRetries: 3,
+  minWait: 1000 * 60 * 10, //10 min wait time after login attempts
+  maxWait: 1000 * 60 * 20, //20 min max wait time
+  lifetime: 1000 * 60 * 20, //20 min lifetime
+})
+
+//login
+router.post('/loginadmin', bruteForce.prevent, loginAdmin)
+
+//sign up
+router.post('/signupadmin', bruteForce.prevent, signupAdmin)
+
+//log out
+router.get('/logoutadmin', logoutAdmin)
+
+module.exports = router
